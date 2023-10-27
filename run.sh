a5b81443e87ec667d319a4a17e2791d6000aaa66
iamtrazy <iamtrazy@protonmail.com>
2023-10-27 07:32:18 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /root/env.sh
 nohup /usr/local/bin/sing-box -D /var/lib/sing-box -C /etc/sing-box run &>/dev/null &
-nohup caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+nohup /usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
